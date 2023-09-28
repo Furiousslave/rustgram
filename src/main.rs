70f9b1c236bd1ceb467cd94623fbd7468e45fb4d
nikitos <xejiuoc@gmail.com>
2023-09-28 20:50:02 +0300

    Added client connection

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io::Write;
+use grammers_client::{Client, Config, InitParams, SignInError};
+use grammers_session::Session;
+use console::{Term, style, Style};
+use grammers_mtsender::AuthorizationError;
+use crate::terminal_colors::GREEN;
+
+mod terminal_colors;
+
 fn main() {
-    println!("Hello, world!");
+    let term = Term::stdout();
+    let a = GREEN.apply_to("Connecting to Telegram...");
+    term.write_line(&*a.to_string()).unwrap_or_else(|error| eprintln!("Error: {}", error));
+
+
+
+
+    // if let Err(error)= result {
+    //     !eprintln!("Error: {}", error)
+    // }
+    // println!("This is {} neat", style("quite").red());
+}
+
+async fn async_main() -> Result<(), AuthorizationError> {
+    let client_config = Config {
+        session: Session::new(),
+        api_id: 22569658,
+        api_hash: "16a2120465917eff8ad394778bb8bfbf".to_string(),
+        // api_hash: "16a2120465917eff8ad394778bb8bfbfffff".to_string(), bad value
+        params: InitParams::default()
+    };
+
+    let client = Client::connect(client_config).await?;
+
+    Ok(())
+    // Ok();
 }
---
+++ src/terminal_colors/mod.rs
@@ -0,0 +1,4 @@
+use console::Style;
+
+pub(crate) static GREEN: Style = Style::new().green();
+pub(crate) static RED: Style = Style::new().red();
